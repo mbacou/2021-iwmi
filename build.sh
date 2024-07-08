7cccc411bd30997ce97afded873e6c2e57baf7b2
Melanie Bacou <mel@mbacou.com>
2024-07-08 21:34:48 +0100

    added newspaper theme, fixed vertical grid

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# description: - build R package
+# description: - build and install R package
 #              - render quarto website
 
 cd "$(dirname "$(realpath "$0")")";
@@ -10,7 +10,7 @@ set -e
 
 # Document and load
 R -e 'roxygen2::roxygenize()'
-R -e 'devtools::load_all()'
+R -e 'devtools::install()'
 
 # Start preview server
 quarto preview . --port 5555
